941caa2a2eb29957485f11effe492f571c5bffe8
Prithvi <prithvin19@gmail.com>
2023-06-03 12:25:21 +0530

    logo

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,22 @@ import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
 import { NextSeo } from "next-seo";
-import { useRef } from "react";
 import FloatingButton from "../components/FloatingButton";
 
 export default function Home() {
-  const videoRef = useRef(null);
-  const eventRef = useRef(null);
-  const campaignRef = useRef(null);
-  const contactRef = useRef(null);
-
   return (
     <div className="text-black font-eczar">
       <NextSeo
-        title="Home: nine4"
+        title="PARS"
         description="PARS ENGINEERING INDIA PVT LTD"
-        canonical="https://nine4-3.vercel.app/"
+        canonical="/"
         openGraph={{
-          url: "https://nine4-3.vercel.app/",
+          url: "/",
         }}
       />
       <Head>
         <title>PARS</title>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.jpg" />
       </Head>
       <Main />
       <FloatingButton />
