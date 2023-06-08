c09b6bd1bfa3a984ff5a8f6cb7d31469ca207c82
Prithvi <prithvin19@gmail.com>
2023-06-08 16:08:05 +0530

    whatsapp

--- components/FloatingButton.js
+++ components/FloatingButton.js
@@ -2,15 +2,19 @@ import React from "react";
 
 export default function FloatingButton() {
   return (
-    <div className="fixed bottom-8 right-4 ">
-      <button>
+    <div className="fixed bottom-8 right-4 " style={{ zIndex: "1000" }}>
+      <a
+        href="https://wa.me/919600520369"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img
           src="/whatsapp.gif"
           alt="msg"
           className="rounded-full p-2 shadow-xl"
-          style={{ boxShadow: "0 0 0 3px #dbc07c8e"}}
+          style={{ boxShadow: "0 0 0 3px #dbc07c8e" }}
         />
-      </button>
+      </a>
     </div>
   );
 }
